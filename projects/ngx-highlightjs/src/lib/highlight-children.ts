969f3b343f4a1015aa89dfca4625d64e4b1a28fd
Murhaf Sousli <murhafsousli@gmail.com>
2019-02-12 14:05:14 +0300

    fix(highlightChildren): not working inside [innerHtml], closes #67

--- projects/ngx-highlightjs/src/lib/highlight-children.ts
+++ projects/ngx-highlightjs/src/lib/highlight-children.ts
@@ -1,18 +1,35 @@
-import { Directive, AfterContentInit, Input, Renderer2, ElementRef, NgZone } from '@angular/core';
+import { Directive, Input, Inject, OnInit, OnDestroy, Renderer2, ElementRef, NgZone, PLATFORM_ID } from '@angular/core';
+import { isPlatformBrowser } from '@angular/common';
 import { HighlightJS } from './highlight.service';
 
 @Directive({
   selector: '[highlightChildren]'
 })
-export class HighlightChildren implements AfterContentInit {
+export class HighlightChildren implements OnInit, OnDestroy {
 
   @Input('highlightChildren') selector: string;
+  private _observer: any;
 
-  constructor(private _zone: NgZone, private _el: ElementRef, private _hljs: HighlightJS, private _renderer: Renderer2) {
+  constructor(private _zone: NgZone,
+              private _el: ElementRef,
+              private _hljs: HighlightJS,
+              private _renderer: Renderer2,
+              @Inject(PLATFORM_ID) private _platformId: Object) {
   }
 
-  ngAfterContentInit() {
-    this.highlightChildren(this.selector);
+  ngOnInit() {
+    if (isPlatformBrowser(this._platformId)) {
+      // Subscribe to host element content changes
+      this._zone.runOutsideAngular(() => {
+        this._observer = new MutationObserver(() =>
+          this.highlightChildren(this.selector)
+        );
+
+        this._observer.observe(this._el.nativeElement, {
+          childList: true
+        });
+      });
+    }
   }
 
   /**
@@ -29,22 +46,20 @@ export class HighlightChildren implements AfterContentInit {
    * @param selector elements selector
    */
   highlightChildren(selector: string) {
-    this._zone.runOutsideAngular(() => {
-      const elementsToHighlight = this._el.nativeElement.querySelectorAll(selector || 'code');
+    const elementsToHighlight = this._el.nativeElement.querySelectorAll(selector || 'pre code');
 
-      elementsToHighlight.forEach((element: HTMLElement) => {
-        // Highlight element when text is present
-        const observer = new MutationObserver(() => {
-          if (
-            element.childNodes.length === 1 &&
-            element.childNodes[0].nodeName === '#text'
-          ) {
-            this.highlightElement(element);
-          }
-          observer.disconnect();
-        });
-        observer.observe(element, {childList: true});
-      });
+    elementsToHighlight.forEach((element: HTMLElement) => {
+      // Highlight element when text is present
+      if (
+        element.childNodes.length === 1 &&
+        element.childNodes[0].nodeName === '#text'
+      ) {
+        this.highlightElement(element);
+      }
     });
+  }
+
+  ngOnDestroy() {
+    this._observer.disconnect();
   }
 }
